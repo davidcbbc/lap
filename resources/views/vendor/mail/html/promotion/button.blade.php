1050cc3ce54c0f81af0605e4c8c57460e16cd3ef
David Capela <davidcapela@MBPdeDavid1517.home>
2020-01-10 15:44:39 +0000

    added email verfic

---
+++ resources/views/vendor/mail/html/promotion/button.blade.php
@@ -0,0 +1,13 @@
+<table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td align="center">
+            <table border="0" cellpadding="0" cellspacing="0" role="presentation">
+                <tr>
+                    <td>
+                        <a href="{{ $url }}" class="button button-green" target="_blank">Verificar E-mail</a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
